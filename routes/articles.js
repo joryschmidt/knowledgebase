162d8a02914d3c99973e33c08eb9fb0979420dd2
Jory Schmidt <joryschmidt1988@gmail.com>
2016-08-22 21:44:56 +0000

    add post put and delete routes

--- routes/articles.js
+++ routes/articles.js
@@ -26,19 +26,46 @@ router.get('/category/:category', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
   // Get form values
-  var title = req.body.title;
-  var category = req.body.category;
-  var body = req.body.body;
+  var title =     req.body.title;
+  var category =  req.body.category;
+  var body =      req.body.body;
   
   // Article object
   var newArticle = new Article({
-    title: title, 
+    title:    title, 
     category: category,
-    body: body
+    body:     body
   })
   
   // Create the article
   Article.createArticle(newArticle, function(err, article) {
+    if (err) console.log(err);
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+});
+
+router.put('/', function(req, res, next) {
+  var id = req.body.id;
+  var data = {
+    title: req.body.title,
+    category: req.body.category,
+    body: req.body.body
+  };
+  
+  // Update the article
+  Article.updateArticle(id, data, function(err, article) {
+    if (err) console.log(err);
+    res.location('/articles');
+    res.redirect('/articles');
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+  var id = req.params.id;
+  
+  // Delete the article
+  Article.removeArticle(id, function(err, article) {
     if (err) console.log(err);
     res.location('/articles');
     res.redirect('/articles');
