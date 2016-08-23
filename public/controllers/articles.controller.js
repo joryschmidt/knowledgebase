1a5449c6f6ac9d8fcaa0d11664b339128a734eb3
Jory Schmidt <joryschmidt1988@gmail.com>
2016-08-23 21:00:00 +0000

    fill in more views

--- public/controllers/articles.controller.js
+++ public/controllers/articles.controller.js
@@ -4,4 +4,11 @@ angular.module('kB')
   $http.get('/articles').success(function(data) {
     $scope.articles = data;
   });
+}])
+
+.controller('ArticlesCategoryCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+  $http.get('/articles/category/'+$routeParams.category).success(function(data) {
+    $scope.cat_articles = data;
+    $scope.category = $routeParams.category
+  });
 }]);
